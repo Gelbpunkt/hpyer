c2b93a1350b420c6f88bf3e202665544404dbb0f
Jens Reidel <adrian@travitia.xyz>
2021-06-14 18:22:42 +0200

    dependency bumps

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export RUSTFLAGS="-C target-cpu=native"
 maturin build --no-sdist --manylinux off --interpreter python3 --release --strip
-pip install target/wheels/*.whl -U
+pip install target/wheels/*.whl -U --force-reinstall
